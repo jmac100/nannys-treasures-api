f1bd96e078c50c71789939f220d79073ec8012d4
jmac100 <jmac_100@hotmail.com>
2019-05-16 09:13:40 -0400

    added picture comments

--- routes/gallery/controller.js
+++ routes/gallery/controller.js
@@ -60,5 +60,66 @@ module.exports = {
     } catch (error) {
       res.status(500).send({ error: error.message })
     }
+  },
+  addComment: async(req, res) => {
+    try {
+      const { comment, auth_token } = req.body
+      if (!auth_token) {
+        return res.status(200).send({ auth: false, msg: 'Missing Token' })
+      }
+
+      const decodedToken = jwt.decode(auth_token)
+      if (!decodedToken) {
+        return res.send({ auth: false, msg: 'Invalid Token' })
+      }
+
+      const member_id = decodedToken.id
+      const member = await memberModel.findById(member_id)
+      if (!member) {
+        return res.send({ success: false, msg: 'Member not found' })
+      }
+
+      const galleryId = req.params.id
+      const gallery = await model.findById(galleryId)
+
+      if (gallery) {
+        comment.member_id = member._id
+        gallery.comments.push(comment)
+        await new model(gallery).save()
+        res.status(200).send({ gallery })
+      } else {
+        res.status(404).send({ msg: 'poem not found' })
+      }
+    } catch (error) {
+      res.status(500).send({ error: error.message })
+    }
+  },
+  removeComment: async(req, res) => {
+    try {
+      const { auth_token } = req.body
+      if (!auth_token) {
+        return res.status(200).send({ auth: false, msg: 'Missing Token' })
+      }
+
+      const decodedToken = jwt.decode(auth_token)
+      if (!decodedToken) {
+        return res.send({ auth: false, msg: 'Invalid Token' })
+      }
+
+      const member_id = decodedToken.id
+      const member = await memberModel.findById(member_id)
+      if (!member) {
+        return res.send({ success: false, msg: 'Member not found' })
+      }
+
+      const gallery_id = req.params.gallery_id
+      const id = req.params.id
+
+      await model.findOneAndUpdate({ _id: gallery_id }, { $pull: { comments: { _id: id, member_id: member._id } } })
+      res.status(200).send({ msg: `Comment ${id} successfully deleted` })
+
+    } catch (error) {
+      res.status(500).send({ error: error.message })
+    }
   }
 }
--- routes/gallery/index.js
+++ routes/gallery/index.js
@@ -4,7 +4,7 @@ const controller = require('./controller')
 router.post('/', controller.create)
 router.get('/', controller.retrieve)
 router.patch('/:id', controller.update)
-// router.post('/:id/comments', controller.addComment)
-// router.delete('/:poem_id/comments/:id', controller.removeComment)
+router.post('/:id/comments', controller.addComment)
+router.delete('/:gallery_id/comments/:id', controller.removeComment)
 
 module.exports = router
